df3fbbc0c6dba56db0e2de935d809a0b4486b7bc
dcorc7 <dc1524@georgetown.edu>
2026-05-21 12:55:23 -0400

    Changed location of GUdomains

--- website/GUdomains.sh
+++ website/GUdomains.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Cleaning previous build..."
-rm -rf website/Soccer-Data-API
+rm -rf Soccer-Data-API
 
 echo "Rendering Quarto website..."
 quarto render website
 
 echo "Setting permissions..."
 
-find website/Soccer-Data-API -type f -exec chmod 644 {} \;
-find website/Soccer-Data-API -type d -exec chmod 755 {} \;
+find Soccer-Data-API -type f -exec chmod 644 {} \;
+find Soccer-Data-API -type d -exec chmod 755 {} \;
 
 read -p "Deploy website to GU Domains? (y/n): " answer
 
@@ -17,8 +17,7 @@ if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
 
     echo "Deploying website..."
 
-    scp -r website/Soccer-Data-API/* \
-    corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
+    scp -r Soccer-Data-API corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
 
     echo "Deployment complete."
 
