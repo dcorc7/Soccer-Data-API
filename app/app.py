1dba8a276ca4c47598691459f01c7f7575ba6af2
dcorc7 <dc1524@georgetown.edu>
2026-06-06 12:56:05 -0400

    Printing goalscorer raw data

--- app/app.py
+++ app/app.py
@@ -298,7 +298,7 @@ with tab3:
     scorers = data["scorers"]
 
     st.write("Raw Test Data:")
-    print(scorers[0])
+    print(scorers)
 
     # Creates table rows with player name, team name, num goals, num penalties
     goals_rows = []
